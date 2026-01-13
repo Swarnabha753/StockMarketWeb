656d296071981a5a7ac7283942c70d0244aab6d6
Swarnabha <nandiswarnabha@gmail.com>
2026-01-14 00:59:51 +0530

    handled logos in productpage

--- frontend/src/landing_page/products/Universe.js
+++ frontend/src/landing_page/products/Universe.js
@@ -1,47 +1,118 @@
-import React from 'react';
+import React from "react";
+
+const partners = [
+    {
+        name: "Smallcase",
+        logo: "media/images/smallcaseLogo.png",
+        description: "Thematic investment platform",
+    },
+    {
+        name: "Streak",
+        logo: "media/images/streakLogo.png",
+        description: "Algo & strategy platform",
+    },
+    {
+        name: "Sensibull",
+        logo: "media/images/sensibullLogo.svg",
+        description: "Options trading platform",
+    },
+    {
+        name: "Zerodha Fund House",
+        logo: "media/images/zerodhaFundhouse.png",
+        description: "Asset management",
+    },
+    {
+        name: "GoldenPi",
+        logo: "media/images/goldenpiLogo.png",
+        description: "Bonds trading platform",
+    },
+    {
+        name: "Ditto",
+        logo: "media/images/dittoLogo.png",
+        description: "Insurance",
+    },
+];
 
 function Universe() {
     return (
-        <div className='container mt-5'>
-            <div className='row text-center'>
-                <h1>The Zerodha Universe</h1>
-                <p>
-                    Extend your tranding and investment experience even further with our partner platforms
+        <div className="container mt-5">
+            <div className="row text-center">
+
+                {/* Heading */}
+                <h1 style={styles.heading}>The Zerodha Universe</h1>
+                <p style={styles.subHeading}>
+                    Extend your trading and investment experience even further with our partner platforms
                 </p>
 
-                <div className='col-4 p-3 mt-5'>
-                    <img src="media\images\smallcaseLogo.png" className="partner-logo" />
-                    <p className='text-small text-muted text-center'>Thematic investment platform</p>
+                {/* Partner Logos */}
+                {partners.map((partner, index) => (
+                    <div
+                        key={index}
+                        className="col-12 col-md-4 p-4 d-flex flex-column align-items-center"
+                    >
+                        <div style={styles.logoWrapper}>
+                            <img
+                                src={partner.logo}
+                                alt={partner.name}
+                                style={styles.logo}
+                            />
+                        </div>
+                        <p style={styles.description}>
+                            {partner.description}
+                        </p>
+                    </div>
+                ))}
+
+                {/* CTA Button */}
+                <div className="col-12">
+                    <button style={styles.button}>
+                        Signup Now
+                    </button>
                 </div>
-                <div className='col-4 p-3 mt-5'>
-                    <img src="media/images/streakLogo.png" className="partner-logo" />
-                    <p className='text-small text-muted text-center'>Algo & strategy platform</p>
-                </div>
-                <div className='col-4 p-3 mt-5'>
-                    <img src="media/images/sensibullLogo.svg" className="partner-logo" />
-                    <p className='text-small text-muted text-center'>Options trading Platform</p>
-                </div>
-                <div className='col-4 p-3 mt-1'>
-                    <img src="media\images\zerodhaFundhouse.png" className="partner-logo" />
-                    <p className='text-small text-muted text-center'>Asset management</p>
-                </div>
-                <div className='col-4 p-3 mt-1'>
-                    <img src="media\images\goldenpiLogo.png" className="partner-logo" />
-                    <p className='text-small text-muted text-center'>Bonds trading platform</p>
-                </div>
-                <div className='col-4 p-3 mt-1'>
-                    <img src="media\images\dittoLogo.png" className="partner-logo" />
-                    <p className='text-small text-muted text-center'>Insurance</p>
-                </div>
-                <button
-                    className="p-2 btn btn-primary fs-5 mb-5"
-                    style={{ width: "20%", margin: "0 auto" }}
-                >
-                    Signup Now
-                </button>
+
             </div>
         </div>
     );
 }
 
 export default Universe;
+
+const styles = {
+    heading: {
+        fontSize: "2.5rem",
+        fontWeight: "600",
+        marginBottom: "8px",
+    },
+    subHeading: {
+        color: "#6c757d",
+        marginBottom: "32px",
+        fontSize: "1rem",
+    },
+    logoWrapper: {
+        height: "60px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        marginBottom: "10px",
+    },
+    logo: {
+        maxHeight: "100%",
+        maxWidth: "100%",
+        objectFit: "contain",
+    },
+    description: {
+        fontSize: "0.9rem",
+        color: "#6c757d",
+        textAlign: "center",
+    },
+    button: {
+        padding: "12px 40px",
+        fontSize: "18px",
+        borderRadius: "6px",
+        border: "none",
+        backgroundColor: "#387ed1",
+        color: "#fff",
+        cursor: "pointer",
+        marginTop: "20px",
+    },
+};
