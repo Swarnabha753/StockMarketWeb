f090c2ac5d8992b66dbf9c717a55ae1b203fcd66
Swarnabha <nandiswarnabha@gmail.com>
2026-01-14 00:33:40 +0530

    Improved product-page and formatted some js files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,18 +16,18 @@ import Login from "./landing_page/login/Login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/product" element={<ProductPage />} />
-      <Route path="/pricing" element={<PricingPage />} />
-      <Route path="/support" element={<SupportPage />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-    <Footer />
-  </BrowserRouter>
+    <BrowserRouter>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/product" element={<ProductPage />} />
+            <Route path="/pricing" element={<PricingPage />} />
+            <Route path="/support" element={<SupportPage />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+    </BrowserRouter>
 );
--- frontend/src/landing_page/products/Universe.js
+++ frontend/src/landing_page/products/Universe.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React from 'react';
 
-function RightSection({ imageURL, productName, productDesription, learnMore }) {
-  return (
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col-6 p-5 mt-5">
-          <h1>{productName}</h1>
-          <p>{productDesription}</p>
-          <div>
-            <a href={learnMore}>Learn More</a>
-          </div>
+function Universe() {
+    return (
+        <div className='container mt-5'>
+            <div className='row text-center'>
+                <h1>The Zerodha Universe</h1>
+                <p>
+                    Extend your tranding and investment experience even further with our partner platforms
+                </p>
+
+                <div className='col-4 p-3 mt-5'>
+                    <img src="media\images\smallcaseLogo.png" className="partner-logo" />
+                    <p className='text-small text-muted text-center'>Thematic investment platform</p>
+                </div>
+                <div className='col-4 p-3 mt-5'>
+                    <img src="media/images/streakLogo.png" className="partner-logo" />
+                    <p className='text-small text-muted text-center'>Algo & strategy platform</p>
+                </div>
+                <div className='col-4 p-3 mt-5'>
+                    <img src="media/images/sensibullLogo.svg" className="partner-logo" />
+                    <p className='text-small text-muted text-center'>Options trading Platform</p>
+                </div>
+                <div className='col-4 p-3 mt-1'>
+                    <img src="media\images\zerodhaFundhouse.png" className="partner-logo" />
+                    <p className='text-small text-muted text-center'>Asset management</p>
+                </div>
+                <div className='col-4 p-3 mt-1'>
+                    <img src="media\images\goldenpiLogo.png" className="partner-logo" />
+                    <p className='text-small text-muted text-center'>Bonds trading platform</p>
+                </div>
+                <div className='col-4 p-3 mt-1'>
+                    <img src="media\images\dittoLogo.png" className="partner-logo" />
+                    <p className='text-small text-muted text-center'>Insurance</p>
+                </div>
+                <button
+                    className="p-2 btn btn-primary fs-5 mb-5"
+                    style={{ width: "20%", margin: "0 auto" }}
+                >
+                    Signup Now
+                </button>
+            </div>
         </div>
-        <div className="col-6">
-          <img src={imageURL} />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
-export default RightSection;
+export default Universe;
